891a77e0aae71759587003a689a1dcb4aaa905b7
Mike <michael.lydon0@gmail.com>
2016-02-08 12:45:52 -0800

    Removed Data from homepage. used controller for data content

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,6 +1,30 @@
 /* GET 'home' page */
 module.exports.homelist = function(req, res){
-  res.render('locations-list', {title: 'Home'});
+  res.render('locations-list', {title: 'Loc8r - find a place to work with wifi',
+                                pageHeader:{
+                                  title: 'Loc8r',
+                                  strapline: 'Find places to work with wifi near you!'
+                                },
+                                locations: [{
+                                  name: 'Starcups',
+                                  address: '125 High Street, Reading, RG6 1PS',
+                                  rating: 3,
+                                  facilities: ['Hot drinks', 'Food', 'Premium Wifi'],
+                                  distance: '100m'
+                                },{
+                                  name: 'Cafe Hero',
+                                  address: '125 High Street, Reading, RG6 1PS',
+                                  rating: 4,
+                                  facilities: ['Hot drinks', 'Food','Premium Wifi'],
+                                  distance: '200m'
+                                },{
+                                  name: 'Burger Queen',
+                                  address: '125 High Street, Reading, RG6 1PS',
+                                  rating: 2,
+                                  facilities: ['Food','Premium Wifi'],
+                                  distance: '250m'
+                                }]
+              });
 };
 
 /* GET 'Location info' page */
